a29f808f8c3d873dc943946f3607c22e87111e1b
BohdanBuhrii <bobuhrii@gmail.com>
2019-05-19 13:17:52 +0300

    added realty

---
+++ DataModels/Realty.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class Realty : IInformable
+    {
+        public long realty_id;
+        public string address;
+        public string district;
+        public string realty_type;
+        //string[] available_services;
+        public string[] available_meters;
+        public string[] ownership;
+        public long owner_id;
+        public long status;
+
+        public string[] GetFieldsName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetFieldsValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
