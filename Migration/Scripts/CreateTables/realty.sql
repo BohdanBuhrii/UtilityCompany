447b1e0a8db083b1374de5486cec42e1c916aaac
BohdanBuhrii <bobuhrii@gmail.com>
2019-05-21 20:14:17 +0300

    realty script added

---
+++ Migration/Scripts/CreateTables/realty.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE realty
+(
+	realty_id bigserial PRIMARY KEY,
+	address char(100),
+	district char(100),
+	realty_type char(50),
+	available_meters bigint[],
+	ownership char(50),
+	owner_id bigserial REFERENCES users(users_id),
+	status char(30)
+
+);
+
