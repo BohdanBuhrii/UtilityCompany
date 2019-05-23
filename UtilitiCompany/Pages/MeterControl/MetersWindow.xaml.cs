9b468d2bc2eb079b1b376a22af06975554ac535f
BohdanBuhrii <bobuhrii@gmail.com>
2019-05-23 18:35:31 +0300

    MeterControl added

---
+++ UtilitiCompany/Pages/MeterControl/MetersWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UtilitiCompany.Pages.MeterControl
+{
+    /// <summary>
+    /// Interaction logic for MetersWindow.xaml
+    /// </summary>
+    public partial class MetersWindow : Window
+    {
+        public MetersWindow(Realty realty)
+        {
+            InitializeComponent();
+        }
+    }
+}
