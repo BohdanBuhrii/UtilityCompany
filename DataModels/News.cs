c2df351751ef2ca4f7593648dcb75198d056f811
BohdanBuhrii <bobuhrii@gmail.com>
2019-05-29 18:25:30 +0300

    added model News

---
+++ DataModels/News.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class News : IInformable
+    {
+        public long Id;
+        public string Title;
+        public string Content;
+        public DateTime CreationDate;
+
+        public string[] GetFieldsName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetFieldsValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
